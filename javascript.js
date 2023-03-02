3dc73706dd73932932654f656c6e03b89f49efa2
Damon Haase <damon.haase@gmail.com>
2023-03-02 05:03:34 -0500

    Start getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice () {
+    let x = Math.floor(Math.random() * 3)
+    switch (x) {
+        case 0:
+            
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+    }
+}
